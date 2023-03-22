9726de6875914549048b727c059e8631a9e17136
FernandoCazrs <2173071926@cua.uam.mx>
2023-03-21 21:28:56 -0600

    Se agrego un efecto al texto del producto blue life 76

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Selecciona el elemento de texto y guárdalo en una variable
+    var texto = document.querySelector('.textoProducto');
+  
+    // Agrega la clase 'mostrar' al elemento de texto después de un retardo de 1 segundo
+    setTimeout(function() {
+      texto.classList.add('mostrar');
+    }, 500);
+  
+});
